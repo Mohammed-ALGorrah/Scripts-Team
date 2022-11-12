28c6ba658c493ac8d55fa3b49d99c9b7db63e226
Abedalaziz-Hmaid <44210428+Abedalaziz-Hmaid@users.noreply.github.com>
2022-11-12 12:42:10 +0200

    Create PlayerMovement Script
    move a player with new input system
    and add small script for camera to follow the player

---
+++ Scripts Team/Assets/Scripts/PlayerScripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.PlayerScripts
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] public Transform target;
+
+        private void LateUpdate()
+        {
+            transform.position = target.position;
+        }
+
+    }
+}
--- Scripts Team/Assets/Scripts/PlayerScripts/PlayerMovment.cs
+++ Scripts Team/Assets/Scripts/PlayerScripts/PlayerMovment.cs
@@ -4,10 +4,60 @@ using UnityEngine;
 
 namespace Scripts.Player
 {
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerMovment : MonoBehaviour
     {
-        
 
+        private CharacterController controller;
+        private Vector3 playerVelocity;
+        private bool groundedPlayer;
+        public float playerSpeed = 5.0f;
+        public float speintSpeed = 8.0f;
+        private float gravityValue = -9.81f;
 
+        private PlayerControls playerControls;
+        private void Awake()
+        {
+            controller = gameObject.GetComponent<CharacterController>();
+            playerControls = new PlayerControls();
+        }
+
+        private void OnEnable()
+        {
+            playerControls?.Enable();
+        }
+
+        private void OnDisable()
+        {
+            playerControls?.Disable();
+        }
+
+        void Update()
+        {
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            Vector3 move = new Vector3(playerControls.Player.Movement.ReadValue<Vector2>().x, 0, playerControls.Player.Movement.ReadValue<Vector2>().y);
+            if (playerControls.Player.Sprint.IsPressed())
+            {
+                controller.Move(move * Time.deltaTime * speintSpeed);
+            }
+            else
+            {
+                controller.Move(move * Time.deltaTime * playerSpeed);
+            }
+            
+
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+        }
     }
 }
