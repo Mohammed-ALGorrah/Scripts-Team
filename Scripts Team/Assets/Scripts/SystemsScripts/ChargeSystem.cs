5e2f08233c24562d81378ae1a7acaf47cdc1617c
Abedalaziz-Hmaid <44210428+Abedalaziz-Hmaid@users.noreply.github.com>
2022-11-12 12:05:12 +0200

    Add Health and charge Scripts

---
+++ Scripts Team/Assets/Scripts/SystemsScripts/ChargeSystem.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Systems
+{
+	public class ChargeSystem : MonoBehaviour
+	{
+		public event Action <ChargeSystem> OnCharageGained;
+		public event Action <ChargeSystem> OnChargeMaxed;
+
+		public int currentCharge = 0;
+		public int maxCharage;
+
+		public void Start()
+		{
+			currentCharge = 0;
+		}
+
+		public ChargeSystem(int max)
+		{
+			this.maxCharage = max;
+		}
+
+		public void IncreaseCharge(int amount)
+		{
+			if (IsChargeFull())
+			{
+				return;
+			}
+			if (currentCharge < maxCharage)
+			{
+				currentCharge += amount;
+				this.OnCharageGained?.Invoke(this);
+
+				if (currentCharge >= maxCharage)
+				{
+					this.OnChargeMaxed.Invoke(this);
+				}
+			}
+		}
+
+		public bool IsChargeFull()
+        {
+			return currentCharge >= maxCharage;
+		}
+
+		public void ResetCharge()
+		{
+			currentCharge = 0;
+		}
+
+	}
+}
---
+++ Scripts Team/Assets/Scripts/SystemsScripts/HealthSystem.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Scripts.SystemsScripts
+{
+    public class HealthSystem : MonoBehaviour
+    {
+        public int currentHealth = 100;
+        public int maxHealth = 100;
+        //bool isDead;
+        public event Action <HealthSystem> OnTakeDamage;
+        public event Action <HealthSystem> OnDead;
+        public event Action <HealthSystem> OnHeal;
+
+        public void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        public HealthSystem(int max)
+        {
+            maxHealth = max;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            if (IsDead())
+            {
+                return;
+            }
+            if (currentHealth > 0 )
+            {
+                currentHealth -= amount;
+                this.OnTakeDamage?.Invoke(this);
+
+                if (currentHealth <= 0)
+                {
+                    this.OnDead.Invoke(this);
+                }
+            }
+            //validate
+            //OnTakeDamage?.Invoke(this);
+
+            // amount <= 0
+            //OnDead?.Invoke(this);
+            //Dead();
+        }
+
+
+        public void Heal(int amount)
+        {
+            currentHealth += amount;
+            if (currentHealth > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+        }
+
+        public bool IsDead()
+        {
+            return currentHealth <= 0;
+        }
+        /*void Dead()
+        {
+            isDead = true;
+        }*/
+
+    }
+}
