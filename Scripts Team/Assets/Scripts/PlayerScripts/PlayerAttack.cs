292ae01e24469b2bdf9bb1b25b6fb070328cee6f
Mohammed ALGorrah <mohammedalgorrah@gmail.com>
2022-11-13 16:23:15 +0200

    .Add Bullets

--- Scripts Team/Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Scripts Team/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -7,38 +7,50 @@ public class PlayerAttack : MonoBehaviour
     
     PlayerType playerType;
 
+    [SerializeField]
+    Transform firePoint;
+
+    public GameObject Bullet;
+
+
     void Start()
     {
         playerType = GetComponent<Player>().playerData.playerType;
+
+        switch(playerType){
+
+            case PlayerType.fighter:
+                Bullet = Resources.Load<GameObject>("BulletsPrefs/Warrior_Ammo");
+                break;
+
+            case PlayerType.defneder:
+            
+                Bullet = Resources.Load<GameObject>("BulletsPrefs/Archer_Ammo");
+
+                break;
+
+            case PlayerType.healther:
+            
+                Bullet = Resources.Load<GameObject>("BulletsPrefs/Wizard_Ammo");
+
+                break;
+           }
     }
     
     
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F)){
+        if(Input.GetKeyDown(KeyCode.R)){
             normalAttack();
         }else if(Input.GetKeyDown(KeyCode.G)){
             specialAttack();
         }
     }
+    
 
     void normalAttack(){
-        switch(playerType){
-
-            case PlayerType.fighter:
-                Debug.Log(playerType);
-                break;
-
-            case PlayerType.defneder:
-                Debug.Log(playerType);
-
-                break;
-
-            case PlayerType.healther:
-                Debug.Log(playerType);
-
-                break;
-           }
+        Instantiate(Bullet,firePoint.position, transform.rotation);
+        
     }
 
     void specialAttack(){
