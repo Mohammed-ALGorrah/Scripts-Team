5f45a291d4dc5fe4e814601e5da5d8d8b93a7e50
Mohammed ALGorrah <mohammedalgorrah@gmail.com>
2022-12-07 19:04:16 +0200

    كلشي شغاااااااال👻👻

--- Scripts Team/Assets/Scripts/GamePlayScripts/Team.cs
+++ Scripts Team/Assets/Scripts/GamePlayScripts/Team.cs
@@ -14,61 +14,27 @@ public class Team : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            /*for (int i = 0; i < playersRed.Count; i++)
-            {
-                Debug.Log("Reeeeeeeed kill " + playersRed[i].GetComponent<PhotonView>().Owner.CustomProperties["team"] );
-
-                Debug.Log("reddddddddd dead " + playersRed[i].GetComponent<PhotonView>().Owner.CustomProperties["dead"]);
-            }
-            for (int i = 0; i < playersBlue.Count; i++)
-            {
-                Debug.Log("Blueeeee kill " +playersBlue[i].GetComponent<PhotonView>().Owner.CustomProperties["team"]);
-
-                Debug.Log("Blueeeee dead " + playersBlue[i].GetComponent<PhotonView>().Owner.CustomProperties["dead"]);
-            }*/
-            foreach (KeyValuePair<int, Photon.Realtime.Player> kvp in PhotonNetwork.CurrentRoom.Players)
-            {
-                Debug.Log("Team = " + kvp.Value.CustomProperties["team"] + "kills = " + kvp.Value.CustomProperties["kills"] 
-                    + "dead = " + kvp.Value.CustomProperties["dead"]);
-            }
-        }
-
         if (txtCounterRed != null)
         {
             currentScoreRed = 0;
             for (int i = 0; i < playersRed.Count; i++)
             {
-                currentScoreRed += (int)playersRed[i].GetComponent<PhotonView>().Owner.CustomProperties["kills"];
+                currentScoreRed += playersRed[i].GetComponent<Player>().numOfKills;
             }
             txtCounterRed.text = currentScoreRed + "";
 
-            //GetComponent<PhotonView>().RPC("setCounterRed",RpcTarget.AllBuffered);
         }
-
 
         if (txtCounterBlue != null)
         {
             currentScoreBlue = 0;
             for (int i = 0; i < playersBlue.Count; i++)
             {
-                currentScoreBlue += (int)playersBlue[i].GetComponent<PhotonView>().Owner.CustomProperties["kills"];
+                currentScoreBlue += playersBlue[i].GetComponent<Player>().numOfKills;
             }
             txtCounterBlue.text = currentScoreBlue + "";
 
-            //GetComponent<PhotonView>().RPC("setCounterBlue",RpcTarget.AllBuffered);
         }
         
-    }
-
-    [PunRPC]
-    public void setCounterRed(){
-        txtCounterRed.text = currentScoreRed + "";
-    }
-
-    [PunRPC]
-    public void setCounterBlue(){
-        txtCounterBlue.text = currentScoreBlue + "";
     }
 }
